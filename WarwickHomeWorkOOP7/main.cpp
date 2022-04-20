e1f6edfe4a5b4d4d382f4fa523ad7f18aac18770
WarwickSeven <WarwickSeven@gmail.com>
2022-04-20 15:35:18 +0300

    clear main

--- WarwickHomeWorkOOP7/main.cpp
+++ WarwickHomeWorkOOP7/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  WarwickHomeWorkOOP7
-//
-//  Created by Александр Тихонов on 20.04.2022.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
